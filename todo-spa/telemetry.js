b7f031d2ee7d1c0aafb54b2637f864809b0209ae
m-arioux <88749079+m-arioux@users.noreply.github.com>
2025-05-27 15:48:23 -0400

    more zipkin (not tested)

--- todo-spa/telemetry.js
+++ todo-spa/telemetry.js
@@ -2,11 +2,16 @@ import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
 import { HostMetrics } from "@opentelemetry/host-metrics";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 
+import { ZipkinExporter } from "@opentelemetry/exporter-zipkin";
 import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import {
   MeterProvider,
   PeriodicExportingMetricReader,
 } from "@opentelemetry/sdk-metrics";
+
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 
 const prometheusPort = 9464;
 
@@ -30,5 +35,21 @@ meterProvider.addMetricReader(new PeriodicExportingMetricReader({ exporter }));
 registerInstrumentations({
   instrumentations: [new HttpInstrumentation(), new HostMetrics()],
 });
+
+if (process.env.ZIPKIN_URL) {
+  const tracerProvider = new NodeTracerProvider();
+
+  const zipkinExporter = new ZipkinExporter({
+    url: process.env.ZIPKIN_URL,
+    serviceName: "todo-spa",
+  });
+
+  tracerProvider.addSpanProcessor(new BatchSpanProcessor(zipkinExporter));
+  tracerProvider.register();
+
+  console.log(`Zipkin tracing enabled: ${process.env.ZIPKIN_URL}`);
+} else {
+  console.log("Zipkin tracing not enabled (set ZIPKIN_URL to activate)");
+}
 
 console.log("Telemetry started");
